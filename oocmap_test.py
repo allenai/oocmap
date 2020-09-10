cfceac36acca78e1e33449aedc117e13f5d85e62
Dirk Groeneveld <dirkg@allenai.org>
2020-09-09 21:38:12 -0700

    Adds a test for LazyList.count()

--- oocmap_test.py
+++ oocmap_test.py
@@ -68,6 +68,11 @@ def test_oocmap_list():
                     lambda: l.index(item, index),
                     lambda: m[0].index(item, index))
 
+        for item in l + ["notfound"]:
+            assert_equal_including_exceptions(
+                lambda: l.count(item),
+                lambda: m[0].count(item))
+
         m[0].append(4)
         assert m[0].eager() == [1, 2.0, "three", m[999], 4]
 
