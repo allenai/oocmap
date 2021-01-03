dababd7f10f73214d5dafe4ab8e262924fc67f83
Dirk Groeneveld <groeneveld@gmail.com>
2021-01-03 00:48:27 -0800

    Adds the LMDB source

--- c/setup.py
+++ c/setup.py
@@ -2,12 +2,16 @@ from distutils.core import setup, Extension
 
 oocmap_module = Extension(
     'oocmap',
-    sources = ['oocmap.c'])
+    sources=[
+        'oocmap.c',
+        'mdb.c',
+        'midl.c'
+    ])
 
-setup (name = 'oocmap',
-       version = '0.1',
-       description = 'The out-of-core map',
-       author = 'Dirk Groeneveld',
-       author_email = 'groeneveld@gmail.com',
-       url = 'http://github.com/dirkgr/oocmap',
-       ext_modules = [oocmap_module])
+setup(name='oocmap',
+    version='0.1',
+    description='The out-of-core map',
+    author='Dirk Groeneveld',
+    author_email='groeneveld@gmail.com',
+    url='http://github.com/dirkgr/oocmap',
+    ext_modules=[oocmap_module])
