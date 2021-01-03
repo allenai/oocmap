02dcec0d8ecc555936a22ccdef1e98e55612b7a8
Dirk Groeneveld <groeneveld@gmail.com>
2021-01-03 14:37:21 -0800

    Bring back most of the code that was crashing in the master branch

--- c/oocmap.c
+++ c/oocmap.c
@@ -1,5 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include "lmdb.h"
 
 static PyObject* oocmap_test(PyObject* const self, PyObject* const args) {
     Py_RETURN_NONE;
@@ -7,17 +8,91 @@ static PyObject* oocmap_test(PyObject* const self, PyObject* const args) {
 
 typedef struct {
     PyObject_HEAD
-    /* Type-specific fields go here. */
+    MDB_env* mdb;
+    size_t currentMapSize;
+    unsigned int transactionCount;
 } OOCMapObject;
 
+static void OOCMap_dealloc(OOCMapObject* self) {
+    mdb_env_close(self->mdb);
+    //(&self->idsWrittenThisTransaction)->~Id2EncodedMap();   // explicit destructor because this was created with placement new
+    Py_TYPE(self)->tp_free((PyObject*)self);
+}
+
+static PyObject* OOCMap_new(PyTypeObject* type, PyObject* args, PyObject* kwds) {
+    OOCMapObject* self = (OOCMapObject*)type->tp_alloc(type, 0);
+    if(self != NULL) {
+        mdb_env_create(&self->mdb);
+        //new(&self->idsWrittenThisTransaction) Id2EncodedMap();  // placement new
+    }
+    return (PyObject*)self;
+}
+
+static int OOCMap_init(OOCMapObject* self, PyObject* args, PyObject* kwds) {
+    // parse parameters
+    static char *kwlist[] = {"filename", NULL};
+    PyObject* filenameObject = NULL;
+    const int parseSuccess = PyArg_ParseTupleAndKeywords(
+            args,
+            kwds,
+            "O&",
+            kwlist,
+            PyUnicode_FSConverter, &filenameObject);
+    if(!parseSuccess)
+        return -1;
+    const char* filename = PyBytes_AS_STRING(filenameObject);
+
+    // open lmdb
+    // These are some aggressive flags that don't guarantee data integrity.
+    const int mdbOpenError = mdb_env_open(
+            self->mdb,
+            filename,
+            MDB_NOSUBDIR | MDB_WRITEMAP | MDB_NOMETASYNC| MDB_MAPASYNC | MDB_NOMEMINIT,
+            0644);
+    if(mdbOpenError != 0) {
+        Py_CLEAR(filenameObject);
+        switch(mdbOpenError) {
+            case MDB_VERSION_MISMATCH:
+                PyErr_Format(PyExc_IOError, "LMDB Error: The version of the LMDB library doesn't match the version that created the database environment.");
+                return -1;
+            case MDB_INVALID:
+                PyErr_Format(PyExc_IOError, "LMDB Error: The environment file headers are corrupted.");
+                return -1;
+            case ENOENT:
+                PyErr_Format(PyExc_IOError, "LMDB Error: The directory specified by the path parameter doesn't exist.");
+                return -1;
+            case EACCES:
+                PyErr_Format(PyExc_IOError, "LMDB Error: The user didn't have permission to access the environment files.");
+                return -1;
+            case EAGAIN:
+                PyErr_Format(PyExc_IOError, "LMDB Error: The environment was locked by another process.");
+                return -1;
+            default:
+                PyErr_Format(PyExc_IOError, "Unknown LMDB error %d", mdbOpenError);
+                return -1;
+        }
+    }
+    MDB_envinfo info;
+    mdb_env_info(self->mdb, &info);
+    self->currentMapSize = info.me_mapsize;
+
+    self->transactionCount = 0;
+
+    // cleanup
+    Py_XDECREF(filenameObject);
+    return 0;
+}
+
 static PyTypeObject OOCMapType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "oocmap.OOCMap",
-    .tp_doc = "The out-of-core map",
-    .tp_basicsize = sizeof(OOCMapObject),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_new = PyType_GenericNew,
+        PyVarObject_HEAD_INIT(NULL, 0)
+        .tp_name = "oocmap.OOCMap",
+        .tp_basicsize = sizeof(OOCMapObject),
+        .tp_itemsize = 0,
+        .tp_dealloc = (destructor)OOCMap_dealloc,
+        .tp_flags = Py_TPFLAGS_DEFAULT,
+        .tp_doc = "The out-of-core map",
+        .tp_init = (initproc)OOCMap_init,
+        .tp_new = OOCMap_new,
 };
 
 static PyMethodDef OocmapMethods[] = {
