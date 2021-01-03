6ddc8432b993341b944db3440c39ca9c8a10defb
Dirk Groeneveld <groeneveld@gmail.com>
2021-01-03 00:55:13 -0800

    Adds an empty OOCMap object

--- c/oocmap.c
+++ c/oocmap.c
@@ -5,6 +5,21 @@ static PyObject* oocmap_test(PyObject* const self, PyObject* const args) {
     Py_RETURN_NONE;
 }
 
+typedef struct {
+    PyObject_HEAD
+    /* Type-specific fields go here. */
+} OOCMapObject;
+
+static PyTypeObject OOCMapType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "oocmap.OOCMap",
+    .tp_doc = "The out-of-core map",
+    .tp_basicsize = sizeof(OOCMapObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_new = PyType_GenericNew,
+};
+
 static PyMethodDef OocmapMethods[] = {
     {"test", oocmap_test, METH_VARARGS, "Make sure the extension module works"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
@@ -19,6 +34,20 @@ static struct PyModuleDef oocmap_module = {
 };
 
 PyMODINIT_FUNC PyInit_oocmap(void) {
-    return PyModule_Create(&oocmap_module);
+    if(PyType_Ready(&OOCMapType) < 0)
+        return NULL;
+
+    PyObject* const m = PyModule_Create(&oocmap_module);
+    if(m == NULL)
+        return NULL;
+
+    Py_INCREF(&OOCMapType);
+    if(PyModule_AddObject(m, "OOCMap", (PyObject*)&OOCMapType) < 0) {
+        Py_DECREF(&OOCMapType);
+        Py_DECREF(m);
+        return NULL;
+    }
+
+    return m;
 }
 
