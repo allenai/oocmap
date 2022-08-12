df883d90a3922f73ce2704d03109126953eefe3e
Dirk Groeneveld <dirkg@allenai.org>
2022-08-12 11:11:38 -0700

    Give some more information in setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,14 @@ oocmap_module = Extension(
 
 setup(name='oocmap',
     version=VERSION,
-    description='The out-of-core map',
+    description='A file-backed dictionary for Python',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author='Dirk Groeneveld',
     author_email='dirkg@allenai.org',
+    license="Apache License 2.0",
+    project_urls={
+      'Source': 'http://github.com/allenai/oocmap'
+    },
     url='http://github.com/allenai/oocmap',
     ext_modules=[oocmap_module])
